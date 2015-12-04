7d5089415207d3bd6dcbab9b00e61fdc572f89c6
Aleksey Storozhev <dark.storoj@gmail.com>
2015-12-04 18:30:05 +0300

    Initial Commit

---
+++ VKText1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKText1
+//
+//  Created by Alexey Storozhev on 04/12/15.
+//  Copyright © 2015 Aleksey Storozhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
